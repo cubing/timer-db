b606e58695806863be95e64e3482c223d4a2a4a8
Lucas Garron <code@garron.net>
2020-08-17 12:51:38 -0700

    Fix build.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "regenerator-runtime/runtime";
 
 export { TimerDB } from "./TimerDB";
-export { Session, SessionCreationOptions } from "./Session";
+export { Session } from "./Session";
+export type { SessionCreationOptions } from "./Session";
 export type { StatListener } from "./Session";
 export type { Attempt, StoredAttempt } from "./data/Attempt";
