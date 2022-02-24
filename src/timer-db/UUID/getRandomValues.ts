bf712836f71687ddac745cb748fd6064e28886bd
Lucas Garron <code@garron.net>
2022-02-24 04:15:55 -0800

    More sad workarounds.

--- src/timer-db/UUID/getRandomValues.ts
+++ src/timer-db/UUID/getRandomValues.ts
@@ -1,12 +1,18 @@
 // From `random-uint-below`.
 
+// Mangled so that bundlers don't try to inline the source.
+const cryptoMangled = "cr-yp-to";
+const cryptoUnmangled = () => cryptoMangled.replace(/-/g, "");
+
 async function nodeGetRandomValues(arr: Uint8Array): Promise<Uint8Array> {
   if (!(arr instanceof Uint8Array)) {
     throw new Error(
       "The getRandomValues() shim only takes unsigned 32-bit int arrays"
     );
   }
-  var bytes = (await import("crypto")).randomBytes(arr.length);
+  var bytes = (await import(/* @vite-ignore */ cryptoUnmangled())).randomBytes(
+    arr.length
+  );
   arr.set(bytes);
   return arr;
 }
